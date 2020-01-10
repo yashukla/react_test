a060b9500a382540a9dca7c70acecb994257feee
Linen <29108833+yashukla@users.noreply.github.com>
2020-01-10 11:13:38 -0800

    Updates

---
+++ precache-manifest.c7fe833021286380465971dc844d5eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48977ce35c8a6bf3de25fa006cc11cb7",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "38d09a4befab25884573",
+    "url": "/react-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cd052b1ef271c529b27e",
+    "url": "/react-test/static/js/2.11be34c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-test/static/js/2.11be34c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38d09a4befab25884573",
+    "url": "/react-test/static/js/main.6fafbe20.chunk.js"
+  },
+  {
+    "revision": "c7a34536796672b2aee9",
+    "url": "/react-test/static/js/runtime-main.a5689f31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
